7226a099f4ec0efcd68dbfc15098d76a622db348
Gerade Geldenhuys <GeradeGeldenhuys@gmail.com>
2020-05-24 00:19:59 +0200

    -added test project

---
+++ Tests/MedPark.Basket.Tests/HandlerTests/CreateBasketHandlerTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedPark.Basket.Tests.HandlerTests
+{
+    public class CreateBasketHandlerTests
+    {
+        public CreateBasketHandlerTests()
+        {
+
+        }
+    }
+
+}
